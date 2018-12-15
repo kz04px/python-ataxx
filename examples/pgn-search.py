3a971593b54b359a3ef76177bcd6e1a8fd36d0d4
Unknown <Twipply@gmail.com>
2018-12-15 15:06:16 +0000

    Example pgn usage
    Find a specified player's games in a .pgn file

---
+++ examples/pgn-search.py
@@ -0,0 +1,15 @@
+import ataxx
+import ataxx.pgn
+
+def main():
+    """
+    Search games.pgn for the specified player 
+    """
+    target = "Player 2"
+    for game in ataxx.pgn.GameIterator("games.pgn"):
+        if target in [game.headers["Black"], game.headers["White"]]:
+            print(game)
+            input()
+
+if __name__ == "__main__":
+    main()
