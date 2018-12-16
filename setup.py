70318e26ec5c0a18f2e31408be0ff9e8c6407cab
Unknown <Twipply@gmail.com>
2018-12-16 19:33:51 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import sys
+
+if sys.version_info < (3, ):
+    raise ImportError('Python 2 is not supported. Consider upgrading to Python 3.')
+
+readme = open('README.md').read()
+
+setup(
+    name = 'ataxx',
+    version = '1.0',
+    license = 'MIT',
+    description = 'python-ataxx is written in Python 3 and supports basic features such as move generation, move validation, engine communication, and board printing.',
+    long_description = readme,
+    author = 'kz04px',
+    url = 'https://github.com/kz04px/python-ataxx',
+    keywords = 'ataxx',
+    packages = ['ataxx'],
+    test_suite = 'test',
+    extras_require = {
+        'draw': 'Pillow'
+    },
+)
